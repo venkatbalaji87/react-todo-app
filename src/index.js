77d787a74bae3634f63d5f9f27997bca8602b69f
venkatbalaji <venkatb2206@gmail.com>
2020-03-29 10:35:52 +0530

    Updated content
    Updated content

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
+//index.html id from app
 const $appRoot = document.querySelector("#app");
 
 ReactDom.render(<App />, $appRoot);
